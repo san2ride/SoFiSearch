e676fe3545f2b926554edfb9db5765d3a68f5286
Jason Sanchez <jason@xao.live>
2019-06-17 14:36:57 -0600

    iOSoFi commit

--- SoFiSearch/SoFiSearch/network/Imgur.swift
+++ SoFiSearch/SoFiSearch/network/Imgur.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Moya
+//import Moya
 
 public enum Imgur {
     
---
+++ SoFiSearch/iOSoFi/iOSoFi/SearchController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchController.swift
+//  iOSoFi
+//
+//  Created by Jason Sanchez on 6/17/19.
+//  Copyright © 2019 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
